0c9618b0cfb312f54f4fd29b054c95123310d3c4
seraphh01 <71792603+seraphh01@users.noreply.github.com>
2022-03-28 09:57:15 +0300

    login-form-update

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-form.component.css']
 })
 export class LoginFormComponent implements OnInit {
-
+  public email?: String;
+  public register: Boolean = false
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
+  public onLogin(){
+    
+  }
+
+  public onRegister(){
+
+  }
 }
