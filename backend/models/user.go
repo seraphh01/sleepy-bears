4df052e04946615527dc037b39cdb376ef95ef9e
SchiauAlessio <alessio.schiau1@gmail.com>
2022-04-08 12:22:27 +0300

    Update user.go
    updated user

--- backend/models/user.go
+++ backend/models/user.go
@@ -1,15 +1,16 @@
 package models
 
 import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
+  "go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type User struct {
-	ID                 primitive.ObjectID `bson:"_id"`
-	Email              *string            `json:"email"`
-	Name               *string            `json:"name"`
-	PhoneNumber        *string            `json:"PhoneNumber"`
-	Enrollments        *[]string          `json:"Enrollments"` // TODO: change from array of strings to array of enrollments
-	Role               *string            `json:"Role"`
-	ProfileDescription *string            `json:"ProfileDescription"`
+  ID                 primitive.ObjectID `bson:"_id"`
+  Username           *string            `json:"Username" validate:"required,min=8,max=32"`
+  Email              *string            `json:"Email" validate:"email,required"`
+  Password           *string            `json:"Password" validate:"required,min=8,max=20"`
+  Name               *string            `json:"Name" validate:"required"`
+  PhoneNumber        *string            `json:"PhoneNumber" validate:"required"`
+  Role               *string            `json:"Role"`
+  ProfileDescription *string            `json:"ProfileDescription"`
 }
