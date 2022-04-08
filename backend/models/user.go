24f655e5246ed98be3ae06f5d48026b135020a0f
SchiauAlessio <alessio.schiau1@gmail.com>
2022-04-08 16:11:35 +0300

    changed constraints

--- backend/models/user.go
+++ backend/models/user.go
@@ -6,9 +6,9 @@ import (
 
 type User struct {
 	ID                 primitive.ObjectID `bson:"_id"`
-	Username           *string            `json:"username" validate:"required,min=8,max=32"`
+	Username           *string            `json:"username" validate:"required,min=6,max=32"`
 	Email              *string            `json:"email" validate:"email,required"`
-	Password           *string            `json:"password" validate:"required,min=8,max=20"`
+	Password           *string            `json:"password" validate:"required,min=8,max=24"`
 	UserType           *string            `json:"usertype" validate:"required,eq=STUDENT|eq=TEACHER|eq=ADMIN|eq=CHIEF"`
 	Name               *string            `json:"name" validate:"required"`
 	ProfileDescription *string            `json:"profiledescription"`
