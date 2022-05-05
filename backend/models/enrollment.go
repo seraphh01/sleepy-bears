ee91900b09f786ccf1cb8f86f5caee13ab9fb708
SchiauAlessio <alessio.schiau1@gmail.com>
2022-05-05 16:02:40 +0300

    Update enrollment.go
    #8 done

--- backend/models/enrollment.go
+++ backend/models/enrollment.go
@@ -4,10 +4,13 @@ import (
   "go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-//TODO list of ints
+type Grade struct {
+  Grade int `json:"grade" validate:"required,min=0,max=10"`
+}
+
 type Enrollment struct {
   ID     primitive.ObjectID `bson:"_id"`
   User   *User              `json:"user" validate:"required"`
   Course *Course            `json:"course" validate:"required"`
-  Grade  *int               `json:"value" validate:"required,min=0,max=10"`
+  Grades []Grade            `json:"grades"`
 }
