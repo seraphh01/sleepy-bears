1bb4bd8fac56f4f18f5b167ffc06cfb63b13456b
SchiauAlessio <alessio.schiau1@gmail.com>
2022-05-19 14:34:31 +0300

    Update course.go
    #43 added credits and semester

--- backend/models/course.go
+++ backend/models/course.go
@@ -1,18 +1,20 @@
 package models
 
 import (
-  "go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Amount struct {
-  Max int `json:"max" validate:"required,min=20"`
+	Max int `json:"max" validate:"required,min=20"`
 }
 
 type Course struct {
-  ID         primitive.ObjectID `bson:"_id"`
-  Name       *string            `json:"name" validate:"required,min=2,max=40"`
-  CourseType *string            `json:"coursetype" validate:"required,eq=MANDATORY|eq=OPTIONAL"`
-  Year       *int               `json:"year" validate:"required,min=1"`
-  Proposer   *User              `json:"proposer" validate:"required"`
-  MaxAmount  *Amount            `json:"maxamount" validate:"required,min=20"`
+	ID         primitive.ObjectID `bson:"_id"`
+	Name       *string            `json:"name" validate:"required,min=2,max=40"`
+	CourseType *string            `json:"coursetype" validate:"required,eq=MANDATORY|eq=OPTIONAL"`
+	Year       *int               `json:"year" validate:"required,min=1"`
+	Proposer   *User              `json:"proposer" validate:"required"`
+	MaxAmount  *Amount            `json:"maxamount" validate:"required,min=20"`
+	Credits    *int               `json:"credits" validate:"required,min=2"`
+	Semester   *int               `json:"semester" validate:"required,min=1,max=2"`
 }
