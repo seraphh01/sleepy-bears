c1288793fb76ac61897b1dfabf4fd882cddbc248
SchiauAlessio <alessio.schiau1@gmail.com>
2022-05-26 11:32:28 +0300

    updated courseController
    #47

--- backend/routes/chiefRouter.go
+++ backend/routes/chiefRouter.go
@@ -1,17 +1,17 @@
 package routes
 
 import (
-  "backend/controllers"
-  "backend/middleware"
+	"backend/controllers"
+	"backend/middleware"
 
-  "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func ChiefRoutes(incomingRoutes *gin.Engine) {
-  incomingRoutes.Use(middleware.Authentication())
-  incomingRoutes.POST("/courses/approve/:id", controllers.ApproveCourse())
-  incomingRoutes.POST("/courses/addmandatory/:teacher", controllers.AddMandatoryCourse())
-  incomingRoutes.GET("/enrollments/bestresults", controllers.GetBestTeacherResults())
-  incomingRoutes.GET("/enrollments/worstresults", controllers.GetWorstTeacherResults())
-
+	incomingRoutes.Use(middleware.Authentication())
+	incomingRoutes.POST("/courses/approve/:id", controllers.ApproveCourse())
+	incomingRoutes.POST("/courses/addmandatory/:teacher", controllers.AddMandatoryCourse())
+	incomingRoutes.GET("/enrollments/bestresults", controllers.GetBestTeacherResults())
+	incomingRoutes.GET("/enrollments/worstresults", controllers.GetWorstTeacherResults())
+	incomingRoutes.GET("/students/average/:courseid", controllers.GetAverageGradeAtCourse())
 }
