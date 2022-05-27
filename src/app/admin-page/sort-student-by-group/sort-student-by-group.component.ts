cc13ba5565aa2e4f297dc8f2a64da2df999a2cdd
916StefEduard <seie3022@scs.ubbcluj.ro>
2022-05-27 18:56:32 +0300

    TeacherCSS

---
+++ src/app/admin-page/sort-student-by-group/sort-student-by-group.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sort-student-by-group',
+  templateUrl: './sort-student-by-group.component.html',
+  styleUrls: ['./sort-student-by-group.component.css']
+})
+export class SortStudentByGroupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
