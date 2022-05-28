98d439b082691ca9d974d91ea2aecde6b95b03eb
seraphh01 <71792603+seraphh01@users.noreply.github.com>
2022-05-28 12:50:21 +0300

    adminPage: View students by group, register Students

--- src/app/admin-page/view-users/view-users.component.ts
+++ src/app/admin-page/view-users/view-users.component.ts
@@ -1,5 +1,12 @@
+import { identifierName } from '@angular/compiler';
 import { Component, Input, OnInit } from '@angular/core';
+import { ObjectId } from 'mongodb';
+import { AdminService } from 'src/app/Services/admin.service';
+import { UserService } from 'src/app/Services/user.service';
+import { Group } from 'src/models/group.model';
 import { Student } from 'src/models/Student';
+import { UserModel } from 'src/models/user.model';
+import { threadId } from 'worker_threads';
 
 @Component({
   selector: 'app-view-users',
@@ -7,11 +14,47 @@ import { Student } from 'src/models/Student';
   styleUrls: ['./view-users.component.css']
 })
 export class ViewUsersComponent implements OnInit {
-  @Input() students: Student[] = [];
-  constructor() { }
+  @Input() students!: UserModel[];
+
+  public groups!: Group[];
+
+  constructor(private adminService: AdminService, private userService: UserService) { }
 
   ngOnInit(): void {
+    this.adminService.addStudentToGroup(915, "seraphh0").subscribe()
+    this.userService.getGroups().subscribe(res => {
+      if(typeof res === 'string')
+        return;
+      this.groups = res;
 
+    }, err => console.error(err));
   }
 
+  public changeGroup(user_name: String, e: any){
+    let group_number = e.target.value;
+
+    this.adminService.addStudentToGroup(group_number, user_name.toString()).subscribe(res => {
+      this.students[this.students.indexOf(this.students.find(s => s.username == user_name)!)].group = res.group;
+      // this.students = this.students.filter(s => s.username != user_name);
+      // this.students.unshift(res);
+    }, err => alert(err));
+  }
+
+  public async viewGroup(e: any){
+    let group: any = e.target.value;
+
+    if(group == 'All'){
+      this.students = await this.adminService.getStudents();
+      return;
+    }
+
+    this.adminService.getStudentsByGroup(group).subscribe(res => {
+      if(typeof res === 'string'){
+        this.students = [];
+        return;
+      }
+      
+      this.students = res;
+    },err => alert(err))
+  }
 }
